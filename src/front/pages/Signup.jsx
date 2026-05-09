fdaf4395e94fd23d8fd4547ccaec90bc2c7773ab
Ana Lopez <avantadesignlab@gmail.com>
2026-05-09 17:55:11 +0000

    feat: create signup view and user registration form

---
+++ src/front/pages/Signup.jsx
@@ -0,0 +1,39 @@
+
+import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
+
+export const Signup = () => {
+    // Access the global state and dispatch function using the useGlobalReducer hook.
+    const { store, dispatch } = useGlobalReducer()
+
+    return (
+        <div className="container">
+            <h1>Registro</h1>
+
+
+            <form>
+                <div class="row mb-3">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputEmail3" />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword3" />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="inputusername" class="col-sm-2 col-form-label">UserName</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputusername" />
+                    </div>
+                </div>
+
+
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+        </div>
+    );
+};
